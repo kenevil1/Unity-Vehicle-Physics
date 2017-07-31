ae794d62d0c933d498c9950f875c034f62d40ab2
ken evil <ken evil>
2017-07-31 10:21:11 +0100

    Version 1.0

---
+++ Assets/Scripts/WheelController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour {
+
+    public WheelAlignment[] steerableWheels;
+
+    //Steering variables
+    public float wheelRotateSpeed;
+    public float wheelSteeringAngle;
+
+    //Motor variables
+    public float wheelAcceleration;
+    public float wheelMaxSpeed;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        wheelControl();
+	}
+
+    //Applies steering and motor torque
+    void wheelControl()
+    {
+        for (int i = 0; i < steerableWheels.Length; i++)
+        {
+            //Sets default steering angle
+            steerableWheels[i].steeringAngle = Mathf.LerpAngle(steerableWheels[i].steeringAngle, 0, Time.deltaTime * wheelRotateSpeed);
+            //Sets default motor speed
+            steerableWheels[i].wheelCol.motorTorque = -Mathf.Lerp(steerableWheels[i].wheelCol.motorTorque, 0, Time.deltaTime * wheelAcceleration);
+
+            //Steering controls
+            if (Input.GetKey(KeyCode.A))
+            {
+                steerableWheels[i].steeringAngle = Mathf.LerpAngle(steerableWheels[i].steeringAngle, -wheelSteeringAngle, Time.deltaTime * wheelRotateSpeed);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                steerableWheels[i].steeringAngle = Mathf.LerpAngle(steerableWheels[i].steeringAngle, wheelSteeringAngle, Time.deltaTime * wheelRotateSpeed);
+            }
+
+            //Motor controls
+            if (Input.GetKey(KeyCode.W))
+            {
+                steerableWheels[i].wheelCol.motorTorque = -Mathf.Lerp(steerableWheels[i].wheelCol.motorTorque, wheelMaxSpeed, Time.deltaTime * wheelAcceleration);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                steerableWheels[i].wheelCol.motorTorque = Mathf.Lerp(steerableWheels[i].wheelCol.motorTorque, wheelMaxSpeed, Time.deltaTime * wheelAcceleration);
+            }
+        }
+    }
+
+}
